2387bb2adc88dd3d742e15aacd65d4d1b413bf30
user.email <talwarpranav929@gmail.com>
2025-07-18 09:08:37 -0600

    intialized express routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,26 @@
-const express = require('express');
+import express from 'express';
+import Jwt  from 'jsonwebtoken';
+ 
+
 const app = express();
+
+app.post('/api/v1/signin', (req, res) => {})
+
+app.post('/api/v1/signup', (req, res) => {})
+
+app.post('/api/v1/content', (req, res) => {})
+
+app.get('/api/v1/content', (req, res) => {})
+
+app.delete('/api/v1/content', (req, res) => {})
+
+app.put('/api/v1/content', (req, res) => {})
+
+app.get('/api/v1/brain/share', (req, res) => {})
+
+app.get('/api/v1/brain/:shareLink', (req, res) => {})
+
+
+
+
+
